372ec3d421f7a431990f9307543fca2c0e57e94a
sachinrtl <108215733+sachinrtl@users.noreply.github.com>
2025-07-12 22:34:20 +0530

    Update tb_d_flip_flop.sv

--- D Flip FLop/tb_d_flip_flop.sv
+++ D Flip FLop/tb_d_flip_flop.sv
@@ -1,6 +1,11 @@
+///////////////////////////////////////////////////
+// Author : Sachin
+// Description : TestBench to simulate D flip flop
+///////////////////////////////////////////////////
+
 module tb_d_flip_flop;
 
-  parameter WDT = 1;
+  parameter WDT = 4;
   
   bit clk;
   bit rst_n;
@@ -9,9 +14,9 @@ module tb_d_flip_flop;
   logic [WDT-1:0] d_out;
 
   d_flip_flop #(
-        .WDT     (1),
-        .RST_VAL (0)
-)
+        .WDT     (WDT),
+        .RESET_VAL (0)
+) u_d_ff_inst
 (
   .clk(clk),
   .rst_n(rst_n),  //Active low reset
@@ -21,15 +26,18 @@ module tb_d_flip_flop;
 );
 
   initial begin
-    rst_n = 1'b1; clk = 1'b0; d_in = 1'b0;
+    rst_n = 1'b1; clk = 1'b0; d_in = WDT'('d0);
     #21 
     rst_n = 1'b0;
     #20
     rst_n = 1'b1;
     #20
-    d_in = 1'b1;
+    @(posedge clk) #1 d_in = WDT'('d2);
+    @(posedge clk) #1 d_in = WDT'('d0);
 
   end
 
+
+  always #20 clk = ~clk;
 
 endmodule
