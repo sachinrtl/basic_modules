73b4686082f9d2ae95e757f41cd9ae37d3c2ba92
sachinrtl <108215733+sachinrtl@users.noreply.github.com>
2025-12-24 22:53:34 +0530

    Update pri_en.sv
    Added synthesis idea for understanding

--- encoder/pri_en.sv
+++ encoder/pri_en.sv
@@ -38,7 +38,50 @@ module pri_en #(
     end
    end
  */ 
-  
   assign valid = |req;
+
+/*---------------------------------------------------------  
+//Synthesis (netlist) --> How priority logic is created in netist
+  grant0 = 0;
+found0 = 0;
+
+// i = 3
+if (req[3] & !found0) begin
+  grant1 = 3;
+  found1 = 1;
+end else begin
+  grant1 = grant0;
+  found1 = found0;
+end
+
+// i = 2
+if (req[2] & !found1) begin
+  grant2 = 2;
+  found2 = 1;
+end else begin
+  grant2 = grant1;
+  found2 = found1;
+end
+
+// i = 1
+if (req[1] & !found2) begin
+  grant3 = 1;
+  found3 = 1;
+end else begin
+  grant3 = grant2;
+  found3 = found2;
+end
+
+// i = 0
+if (req[0] & !found3) begin
+  grant4 = 0;
+  found4 = 1;
+end else begin
+  grant4 = grant3;
+  found4 = found3;
+end
+
+grant = grant4;
+-------------------------------------------------------*/
   
 endmodule
