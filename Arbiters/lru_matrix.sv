ea735ee852da70f26b4c643115f2a7d9c496c3c1
sachinrtl <108215733+sachinrtl@users.noreply.github.com>
2025-12-31 20:46:32 +0530

    Update lru_matrix.sv

--- Arbiters/lru_matrix.sv
+++ Arbiters/lru_matrix.sv
@@ -10,6 +10,8 @@ module lru_matrix #(
   output logic [N-1:0] grant
 );
 
+   //If lru_matrix[i][j] == 1'b1 , it means i is lru against j. for it to get grant lru_matrix[i][j] == 1'b1 for all j except i==j.
+   //For example soon out of reset req 3gets grant in case of all reqs because with reset values in matrix only [3][0], [3][1], [3][2] are 1'b1
   logic [N-1:0][N-1:0] lru_matrix;
 
   logic oldest;
@@ -17,8 +19,8 @@ module lru_matrix #(
 
   always_comb begin
     grant = '0;
-    oldest = 1'b1;
-    found = 1'b0;
+    oldest = 1'b1;     //Start with assumption that i is the least recently used unless proved otherwise
+    found = 1'b0;      //to break the check once LRU requetor is identified 
     if(req[i]) begin
       for(int j=0; i<N; i++) begin
         if(i!=j && req[j] && !lru_matrxi[i][j])
@@ -39,12 +41,12 @@ module lru_matrix #(
     if(!rst_n) begin
       for(int i=0; i<N; i++) begin //for1
         for(int j=0; j<N; j++) begin //for2
-          lru_marix[i][j] <= (i>j) ? 1'b1 : 1'b0;
+           lru_marix[i][j] <= (i>j) ? 1'b1 : 1'b0;     //fill tha matrxi with 1'b1 if i>j    for initial ordering. This will start with req[3] incase of all reqs valid soon after reset. To start with req[0] use i<j 
         end//for2
       end//for1
     end else begin
       for(i=0; i<N; i++) begin //for1
-        if(grant[i]) begin  
+         if(grant[i]) begin  //Once certain req is granted update the matrix
           for(j=0; j<N; j++) begin //for2
             if(i!=j) begin
               lru_matrix[i][j] <= 1'b0;
